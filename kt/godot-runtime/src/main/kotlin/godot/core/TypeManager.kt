baf5170540d16f3766493d564d201f1eb74d6f4d
Pierre-Thomas Meisels <dev001@infra.utopia-rise.com>
2020-10-01 09:19:38 +0200

    Adapt register engine types for jvm

--- kt/godot-runtime/src/main/kotlin/godot/core/TypeManager.kt
+++ kt/godot-runtime/src/main/kotlin/godot/core/TypeManager.kt
@@ -2,12 +2,13 @@ package godot.core
 
 object TypeManager {
     private val userTypes = HashSet<String>()
+    private val engineTypes = HashSet<String>()
 
-    fun registerUserType(className: String) {
-        userTypes.add(className)
-    }
+    fun registerUserType(className: String) = userTypes.add(className)
 
-    fun isUserType(className: String): Boolean {
-        return userTypes.contains(className)
-    }
+    fun registerEngineType(className: String) = engineTypes.add(className)
+
+    fun isUserType(className: String) = userTypes.contains(className)
+
+    fun isEngineType(className: String) = engineTypes.contains(className)
 }
